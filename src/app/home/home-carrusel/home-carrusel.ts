6293146fa3cdcca07d23bb5249d87841d949dcc1
Alexis Perez <alexisperez.1120@gmail.com>
2026-04-07 22:52:50 -0400

    Feat: Creacion de avisos con firebase

--- src/app/home/home-carrusel/home-carrusel.ts
+++ src/app/home/home-carrusel/home-carrusel.ts
@@ -1,26 +1,39 @@
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  inject
+} from '@angular/core';
 import { SwiperContainer } from 'swiper/element';
 import { SwiperOptions } from 'swiper/types';
-
-
+import { AvisosService, Aviso } from '../../services/avisos.service';
 
 @Component({
   selector: 'app-home-carrusel',
   standalone: true,
   imports: [],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home-carrusel.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeCarrusel implements OnInit {
+  listaAvisos: Aviso[] = [];
+  
+  private avisosService = inject(AvisosService);
+  private cdr = inject(ChangeDetectorRef);
 
   //Referencia de Carrousel de Swiper
-  @ViewChild('SwiperCarrousel', {static: true}) SwiperCarrousel?: ElementRef<SwiperContainer>;
+  @ViewChild('SwiperCarrousel', { static: true })
+  SwiperCarrousel?: ElementRef<SwiperContainer>;
 
-  ngOnInit(){
+  ngOnInit() {
     //Inicializacion de propiedades de carrusel
     const opcionesSwiper: SwiperOptions = {
-      injectStyles:[
+      injectStyles: [
         `
         :host {
           --swiper-navigation-color: gray;
@@ -35,23 +48,30 @@ export class HomeCarrusel implements OnInit {
           opacity:0.5;
         }
 
-        `
+        `,
       ],
       slidesPerView: 1,
-      speed:500,
+      speed: 500,
       loop: true,
-      navigation: true
+      navigation: true,
     };
 
-    if(this.SwiperCarrousel){
+    if (this.SwiperCarrousel) {
       Object.assign(this.SwiperCarrousel!.nativeElement, opcionesSwiper);
-      this.SwiperCarrousel.nativeElement.initialize();
     }
 
+    this.avisosService.getAvisos().subscribe((avisos) => {
+      this.listaAvisos = avisos;
+      this.cdr.markForCheck();
+      
+      // Init or update swiper once DOM is updated
+      setTimeout(() => {
+        if (this.SwiperCarrousel && !this.SwiperCarrousel.nativeElement.swiper) {
+          this.SwiperCarrousel.nativeElement.initialize();
+        } else if (this.SwiperCarrousel?.nativeElement.swiper) {
+          this.SwiperCarrousel.nativeElement.swiper.update();
+        }
+      });
+    });
   }
-
-
-
-
-
- }
+}
